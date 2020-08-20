3458de92766034e8e0044c1ae532b7d1e422fedc
Jeffrey Liu <17771770679@163.com>
2020-08-20 20:56:10 +0800

    10.4获取列表内的影视信息并将其存储，同时存储影视的详情信息

--- ppt10/jobs/tasks/movie.py
+++ ppt10/jobs/tasks/movie.py
@@ -1,8 +1,9 @@
 from flask_script import Command
 from application import app,db
-import requests,os,time
+import requests,os,time,hashlib,json
 from bs4 import BeautifulSoup
 from common.libs.DateHelper import getCurrentTime
+from urllib.parse import urlparse
 
 class JobTask( Command ):
   def __init__(self):
@@ -34,6 +35,7 @@ class JobTask( Command ):
 
     # 获取的页码
     pages = range(1,config['num']+1)
+    # 获取列表的信息
     for ind in pages:
       # 列表路径
       tmp_path = path_list + '/' + str(ind)
@@ -43,14 +45,57 @@ class JobTask( Command ):
         continue
       tmp_content = self.getHttpContent(tmp_url)
       self.saveContent(tmp_path,tmp_content)
-      # app.logger.info("tmp_content：",tmp_content)
-      # 没获取完一个页面，停顿0.3秒，防止反爬虫
+      # 每获取完一个页面，停顿0.3秒，防止反爬虫
       time.sleep(0.3)
 
     for ind in os.listdir(path_list):
+      # 列表内容
       tmp_content = self.getContent(path_list + '/' + str(ind))
-      # app.logger.info("tmp_content：",tmp_content)
-      # print("tmp_content：",tmp_content)
+      # 获取列表内的影视信息
+      items_data = self.parseList(tmp_content)
+      if not items_data:
+        continue
+      # 将列表影视信息用json存起来
+      for item in items_data:
+        tmp_json_path = path_json + '/' + item['hash']
+        tmp_info_path = path_info + '/' + item['hash']
+        if not os.path.exists(tmp_json_path):
+          self.saveContent(tmp_json_path,json.dumps(item,ensure_ascii=False))
+        # 存储影视的详情信息
+        if not os.path.exists(tmp_info_path):
+          tmp_content = self.getHttpContent(item['url'])
+          self.saveContent(tmp_info_path,tmp_content)
+
+        time.sleep(0.3)
+
+      # app.logger.info("tmp_content ：",tmp_content)
+
+  # 解析列表数据
+  def parseList(self,content):
+    data = []
+    config = self.url
+    url_info = urlparse(config['url'])
+    # 获取到域名，和列表数据提供的相对路径结合可以得到完整的url地址
+    url_domain = url_info[0]+'://'+url_info[1]
+
+    tmp_soup = BeautifulSoup(str(content),'html.parser')
+    tmp_list = tmp_soup.select("div.list_su ul li")
+    for item in tmp_list:
+      tmp_target = item.select("a.pic_link")
+      tmp_name = tmp_target[0]["title"]
+      tmp_href = tmp_target[0]["href"]
+      if 'http:' not in tmp_href:
+        tmp_href = url_domain + tmp_href
+
+      tmp_data = {
+        'name':tmp_name,
+        'url':tmp_href,
+        'hash':hashlib.md5(tmp_href.encode('utf-8')).hexdigest(),
+      }
+      data.append(tmp_data)
+
+    # print("data ：",data)
+    return data
 
   # 获取文件内容
   def getContent(self,path):
@@ -68,7 +113,7 @@ class JobTask( Command ):
         f.write(content)
         f.flush()
         f.close()
-    pass
+    
   def getHttpContent(self,url):
     try:
       r = requests.get(url)
