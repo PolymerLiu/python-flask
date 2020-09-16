bb32f7d7cc3f4b0d89bcfeab95619d17c8e6fd61
Jeffrey Liu <17771770679@163.com>
2020-09-16 22:53:52 +0800

    11 影视列表和详情实现

--- ppt11/common/libs/Helper.py
+++ ppt11/common/libs/Helper.py
@@ -1,4 +1,5 @@
 from flask import jsonify,g,render_template
+import math
 
 
 def ops_render(template,context = {}):
@@ -13,3 +14,28 @@ def ops_renderJSON(code=200, msg='操作成功~~', data={}):
 
 def ops_renderErrJSON(msg='系统繁忙，请稍后再试~~', data={}):
   return ops_renderJSON(code=-1, msg=msg, data=data)
+
+def iPagination(params):
+  total_count = int(params['total_count'])
+  page_size = int(params['page_size'])
+  page = int(params['page'])
+  url = params['url']
+
+  total_pages = math.ceil(total_count/page_size)
+  total_pages = total_pages if total_pages>0 else 1
+
+  is_prev = 0 if page <= 1 else 1
+  is_next = 0 if page >= total_pages else 1
+
+  pages={
+    'page_size':page_size,
+    'total':total_count,
+    'total_pages':total_pages,
+    'range':range(1,total_pages+1),
+    'is_prev':is_prev,
+    'is_next':is_next,
+    'current':page,
+    'url':url,
+  }
+  
+  return pages
